3d9482449b36f54fa1d7b4c54c7a263133a9a7ee
jameslear99 <jameslear99@gmail.com>
2023-04-05 09:54:29 -0600

    initial Commit

---
+++ backend/JoelAPI/Controllers/Data/JoelHiltonMovieCollectionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JoelAPI.Controllers
+{
+
+
+
+    public class JoelHiltonMovieCollectionContext : DbContext
+    {
+        public JoelHiltonMovieCollectionContext(DbContextOptions<JoelHiltonMovieCollectionContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
+
+
---
+++ backend/JoelAPI/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoelAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        public JoelHiltonMovieCollectionContext context;
+        public MovieController(JoelHiltonMovieCollectionContext temp)
+        {
+            context = temp;
+        }
+        public IEnumerable<Movie> Get()
+        {
+            var x = context.Movies.ToArray();
+            return x;
+        } 
+    }
+}
